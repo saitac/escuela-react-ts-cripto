2050f19cb2045884eeca7cb80fd6a052c1cf97fe
salvador <salvadoraita@gmail.com>
2024-08-20 18:14:52 -0400

    20240820 18:14

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,20 @@
 function App() {
   return (
     <div
-      className="max-w-5xl m-0"
+      className="m-0 bg-slate-300 max-auto"
     >
-      <h1>Cripto App</h1>
+      <div className="max-w-4xl bg-yellow-200">
+        <h1
+          className="text-4xl my-10 text-white text-center"
+        >
+          Cotizador de <span className="block text-[#61ECBC] text-5xl">Criptomonedas</span>
+        </h1>
+        <div className="my-20 py-24 px-8 bg-white">
+            sa
+
+        </div>
+      </div>
+
     </div>
   )
 }
